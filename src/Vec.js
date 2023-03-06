d2d9037e0216513c70d5eb8b55d452cf554c6202
jacobwmorris <jacobwmorris0@gmail.com>
2023-03-06 16:03:59 -0500

    Changed Vec.inBounds to be exclusive of b

--- src/Vec.js
+++ src/Vec.js
@@ -12,7 +12,7 @@ const Vec = {
     },
 
     inBounds: function(pos, a, b) {
-        return pos[0] >= a[0] && pos[0] <= b[0] && pos[1] >= a[1] && pos[1] <= b[1]
+        return pos[0] >= a[0] && pos[0] < b[0] && pos[1] >= a[1] && pos[1] < b[1]
     }
 }
 
